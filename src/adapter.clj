480378992072853a5fbe0033c2ab1e215b96ff6b
wevre <6877948+wevre@users.noreply.github.com>
2023-05-14 22:58:41 -0600

    transit success

---
+++ src/adapter.clj
@@ -0,0 +1,31 @@
+(ns adapter
+  (:require [cognitect.transit :as transit]
+            [ring.adapter.jetty :as jetty])
+  (:import (java.io ByteArrayOutputStream ByteArrayInputStream)))
+
+(defn handler [request]
+  ;; decode body of request as transit data,
+  ;; add the two numbers and return them.
+  (let [reader (transit/reader (:body request) :json)
+        {:keys [num1 num2]} (transit/read reader)]
+
+    {:status 200
+     :headers {"Content-Type" "application/transit+json"}
+     :body (let [baos (ByteArrayOutputStream.)
+                 writer (transit/writer baos :json)]
+             (transit/write writer {:result (format "%.1f+%.1f=%.1f" num1 num2 (+ num1 num2))})
+             (.toByteArray baos))}))
+
+(defonce adapter (atom nil))
+
+(defn go []
+  (reset! adapter (jetty/run-jetty handler {:port 8860 :join? false})))
+
+(defn halt! []
+  (.stop @adapter)
+  (reset! adapter nil))
+
+(comment
+  (go)
+  (halt!)
+  )
